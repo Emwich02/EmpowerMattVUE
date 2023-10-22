722fc60006783122ebfb0242f66c0a3125d361ca
wishmedew <115174006+wishmedew@users.noreply.github.com>
2023-10-22 21:03:01 +0700

    [home] first block text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,33 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import officepic from '@/images/officepic3.png'; // Replace with the correct path to your image
+</script>
 <template>
-  <div class="background">
-    <div class="container">
-      <h1 class="text">Welcome to Empower</h1>
-      <h4 class="text">Empower Material Innovations</h4>
-      <p class="text">
-        As a Platform Our approach to developing and creating empower material innovations steps up to Intellectual
-        Properties(IP).
-        We focus on doing the research and development in sustainable materials, which will be the eco-friendly products
-        from hemp composite materials to drive the innovation in your lifestyle. We are also doing the development of
-        graphene manufacturing processes for innovation products. We have some partners on unique opportunities to go
-        throughout the same goal.
-        Thank you very much for your kindness.
-      </p>
-    </div>
-  </div>
-</template>
+        <div class="picture">
+            
+        <div class="text-center">
+            <h1 class="welcome-text">Welcome to <span class="empower-text">Empower</span></h1>
+            
+             </div> <br>
 
+
+            <div class="box" >
+                <h4 class="text-center" style="">Empower Material Innovations </h4>
+            </div><br><br>
+        
+    
+        <div class="container">   
+            
+            <p class="text-center">As a Platform Our approach to developing and creating empower material innovations steps up to Intellectual Properties(IP).<br>
+                  We focus on doing the research and development in sustainable materials, which will be the eco-friendly products from hemp composite materials to drive the innovation in your lifestyle. We are also doing the development of graphene manufacturing processes for innovation products. 
+                  <br>We have some partners on unique opportunities to go throughout the same goal. 
+                Thank you very much for your kindness. 
+            </p>
+         </div> 
+        </div>  
+</template>
+<style lang="scss">
+// Import Main styles for this application
+@import '/src/assets/home.scss';
+</style>
 
