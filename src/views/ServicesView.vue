fef27f15afc6c1c7830aa067d0ae985a8fd557d0
teerat.s <teerat.sri89@gmail.com>
2023-10-23 12:47:32 +0700

    [contactUs , Services , Team] Add variable for label and update background image.

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,38 +1,39 @@
-<template>
-    <link href="./asset/services.css" rel="stylesheet" type="text/css" />
+<script setup>
 
-<div class="container mb-5" style="padding-top: 10rem;">
-    <div class="col-3 bg-head d-flex align-items-center justify-content-center">
-        <p style="font-size: 1.875vw;">Our services</p>
-    </div>
-    <div class="row">
-        <div class="col-lg-6 text-head-box">
-            <p class="head-text">Research & Consultant services</p>
-            <p class="detail-text">Research and development in composite and green materials including nanotechnology.
-            </p>
-            <p class="detail-text">Consultant for an innovation project in the field of composite and green materials
-                including
-                nanotechnology.</p>
+</script>
+<template>
+    <div class="container mb-5" style="padding-top: 10rem;">
+        <div class="col-3 bg-head d-flex align-items-center justify-content-center">
+            <p style="font-size: 1.875vw;">Our services</p>
         </div>
-        <div class="col-lg-6 row">
-            <div class="col-5 bg-image-research d-flex align-items-center justify-content-center">
-                <p class="services-text">Research meterials for ‘<span class="hemp-car-text">Hemp Car</span>’</p>
+        <div class="row">
+            <div class="col-lg-6 text-head-box">
+                <p class="head-text">Research & Consultant services</p>
+                <p class="detail-text">Research and development in composite and green materials including nanotechnology.
+                </p>
+                <p class="detail-text">Consultant for an innovation project in the field of composite and green materials
+                    including
+                    nanotechnology.</p>
             </div>
-            <div class="col-5 bg-image-consult d-flex align-items-center justify-content-center">
-                <p class="services-text">Research & Consultant service</p>
+            <div class="col-lg-6 row">
+                <div class="col-5 bg-image-research d-flex align-items-center justify-content-center">
+                    <p class="services-text">Research meterials for ‘<span class="hemp-car-text">Hemp Car</span>’</p>
+                </div>
+                <div class="col-5 bg-image-consult d-flex align-items-center justify-content-center">
+                    <p class="services-text">Research & Consultant service</p>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<hr class="line-section" size="5px">
-<div class="bg-image-hempcar">
-    <!-- {{!-- <img src="/images/hempcar-bg.png" alt="hempcar" style="width: 100%;"> --}} -->
-</div>
-<div class="bg-green-color">
-    <div class="container opacity-container">
-        test
+    <hr class="line-section" size="5px">
+    <div class="bg-image-hempcar">
+        <!-- {{!-- <img src="/images/hempcar-bg.png" alt="hempcar" style="width: 100%;"> --}} -->
     </div>
-</div>
+    <div class="bg-green-color">
+        <div class="container opacity-container">
+            test
+        </div>
+    </div>
 </template>
 <style lang="scss">
 // Import Main styles for this application
