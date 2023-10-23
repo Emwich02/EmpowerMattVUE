45b20b436dfacf1a275cc812518b50705901b45f
teerat.s <teerat.sri89@gmail.com>
2023-10-23 22:05:26 +0700

    [Service] Add Spec and properties of hemp car.

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 const Data = {
     MainTitle: "Our services",
     MainDetail_1: "Research & Consultant services",
@@ -8,6 +9,11 @@ const Data = {
     Research_2: "Research & Consultant service",
     Research_Detail_1: "Empowermatt Co.,Ltd. and YATT Co., Ltd. are doing the development of electric automotive. The energy is an electrical battery and the body is produced by hemp fiber reinforced polymer as called ‘Hemp Car’. By the way, we will never stop to develop our products, and we will update our progressing project as soon as possible. It is coming soon, Thank you.",
     Spec_Hempcar: "Specification of",
+    Prop_Hempcar: "Properties of materials for ",
+    Prop_Hempcar_Lab: "Testing Laboratory : GALAXI (GISTDA’s Aerospace Laboratory of Excellence and Innovation),GISTDA.",
+    Prop_Hempcar_Method: "Test method :"
+}
+const listdata_1 = ref({
     Spec_Detail_Hempcar_1: "Body Material: Structural car body made with Hemp Fiber Composite Material",
     Spec_Detail_Hempcar_2: "Electrical System: Rechargeable electric motor with BLDC (Brushless DC) 1,000 W, 60 V",
     Spec_Detail_Hempcar_3: "Controller: EM30s",
@@ -21,71 +27,81 @@ const Data = {
     Spec_Detail_Hempcar_11: "Dimensions: 105 cm width x 210 cm length x 160 cm height",
     Spec_Detail_Hempcar_12: "Color Options: Light blue, white, black, and natural hemp fiber",
     Spec_Detail_Hempcar_13: "Decorative Art: Customizable for customers with an additional charge",
-    Prop_Hempcar: "Properties of materials for",
-    Prop_Hempcar_Lab: "Testing Laboratory ",
-    Prop_Hempcar_Method: "Test method"
-}
+})
+
+const mech_list = ref({
+    Prop_Mech_1 : "Tensile strength [MPa]",
+    Prop_Mech_2 : "Ultimate tensile strain [usn]"
+})
 </script>
 <template>
-    <div class="container mb-5" style="padding-top: 10rem;">
-        <div class="col-3 bg-head d-flex align-items-center justify-content-center">
-            <p style="font-size: 1.875vw;">{{ Data.MainTitle }}</p>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 text-head-box">
-                <p class="head-text">{{ Data.MainDetail_1 }}</p>
-                <p class="detail-text">{{ Data.MainDetail_2 }}</p>
-                <p class="detail-text">{{ Data.MainDetail_3 }}</p>
+    <div class="continer-fluid bg-container">
+        <div class="container mb-5 bg-container" style="padding-top: 10rem;">
+            <div class="col-3 bg-head d-flex align-items-center justify-content-center">
+                <p class="title-text">{{ Data.MainTitle }}</p>
             </div>
-            <div class="col-lg-6 row">
-                <div class="col-5 bg-image-research d-flex align-items-center justify-content-center">
-                    <p class="services-text">{{ Data.Research_1 }}<span class="hemp-car-text">Hemp Car</span>’</p>
+            <div class="row">
+                <div class="col-lg-6 text-head-box">
+                    <p class="head-text">{{ Data.MainDetail_1 }}</p>
+                    <p class="detail-text">{{ Data.MainDetail_2 }}</p>
+                    <p class="detail-text">{{ Data.MainDetail_3 }}</p>
                 </div>
-                <div class="col-5 bg-image-consult d-flex align-items-center justify-content-center">
-                    <p class="services-text">{{ Data.Research_2 }}</p>
+                <div class="col-lg-6 row ">
+                    <div class="col-1"></div>
+                    <div class="col-5 bg-image-research p-0"
+                        :style="{ backgroundImage: 'url(src/images/hempcar-art.png)' }">
+                        <div class="container-fluid bg-image-research d-flex align-items-center justify-content-center m-0">
+                            <p class="services-text">{{ Data.Research_1 }}<span class="hemp-car-text">Hemp Car</span>’</p>
+                        </div>
+
+                    </div>
+                    <div class="col-5 bg-image-consult p-0"
+                        :style="{ backgroundImage: 'url(src/images/solar-sheet-1.jpg)' }">
+                        <div class="container-fluid bg-image-consult d-flex align-items-center justify-content-center m-0">
+                            <p class="services-text">{{ Data.Research_2 }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <hr class="line-section-1" size="5px">
     </div>
-    <hr class="line-section" size="5px">
+
     <div class="bg-image-hempcar">
         <!-- {{!-- <img src="/images/hempcar-bg.png" alt="hempcar" style="width: 100%;"> --}} -->
     </div>
+    
     <div class="bg-green-color">
-        <div class="container opacity-container">
-            <div>
-                <p>Hemp Car</p>
-                <p>{{ Data.Research_Detail_1 }}</p>
+        <div class="container opacity-container ">
+            <!-- Spec hemp car -->
+            <div class="row pt-5 mb-5">
+                <div class="col-1"></div>
+                <div class="col-5">
+                    <img src="/src/images/hempcar.jpg" alt="" class="hempcar-photo">
+                </div>
+                <div class="col-5">
+                    <p class="hempcar-head-text">Hemp Car</p>
+                    <p class="hempcar-detail-text">{{ Data.Research_Detail_1 }}</p>
+                </div>
             </div>
-            <div>
-                <p>{{ Data.Spec_Hempcar }} ‘<span class="hemp-car-text">Hemp Car</span>’</p>
+            <div class="m-spec">
+                <p class="Spec-main-text">{{ Data.Spec_Hempcar }} ‘<span class="hemp-car-text">Hemp Car</span>’</p>
                 <ul>
-                    <li>{{ Data.Spec_Detail_Hempcar_1 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_2 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_3 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_4 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_5 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_6 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_7 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_8 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_9 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_10 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_11 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_12 }}</li>
-                    <li>{{ Data.Spec_Detail_Hempcar_13 }}</li>
+                    <li class="Spec-detail-text"  v-for="(value, key) in listdata_1" :key="key">
+                        {{ value }}
+                    </li>
                 </ul>
                 <p>video hemp car</p>
             </div>
-            <div>
-                <p>{{ Data.Prop_Hempcar }} ‘<span class="hemp-car-text">Hemp Car</span>’</p>
-                <p>{{ Data.Prop_Hempcar_Lab }}</p>
-                <p>{{ Data.Prop_Hempcar_Method }} : GALAXI (GISTDA’s Aerospace Laboratory of Excellence and Innovation),
-                    GISTDA</p>
-                <p>{{ Data.Prop_Hempcar_Method }} :</p>
-                <p>1. ASTM D3039/D3039M-17 : Standard test method for tensile properties of </p>
-                <p>polymer matrix composite materials</p>
-                <p>2. ASTM D790-17 : Standard test method for flexural properties of </p>
-                <p>unreinforced and reinforced plastics and electrical</p>
+            <!-- Prop hemp car -->
+            <div class="m-prop">
+                <p class="Prop-main-text">{{ Data.Prop_Hempcar }} ‘<span class="hemp-car-text">Hemp Car</span>’</p>
+                <p class="Prop-detail-text">{{ Data.Prop_Hempcar_Lab }}</p>
+                <p class="Prop-detail-text">{{ Data.Prop_Hempcar_Method }}</p>
+                <p class="Prop-Method-detail-text-1">1. ASTM D3039/D3039M-17 : </p>
+                <p class="Prop-Method-detail-text-2">Standard test method for tensile properties of polymer matrix composite materials.</p>
+                <p class="Prop-Method-detail-text-1">2. ASTM D790-17 : </p>
+                <p class="Prop-Method-detail-text-2">Standard test method for flexural properties of unreinforced and reinforced plastics and electrical.</p>
                 <table>
                     <tr>
                         <th>Mechanical and physical properties</th>
@@ -93,30 +109,68 @@ const Data = {
                         <th>Standard method</th>
                     </tr>
                     <tr>
-                        <td>John</td>
-                        <td>Admin</td>
-                        <td>North</td>
+                        <td>Tensile strength [MPa]</td>
+                        <td>34.445±1.194</td>
+                        <td>ASTM D3039/D3039M-17</td>
                     </tr>
                     <tr>
-                        <td>Smith</td>
-                        <td>Logistics</td>
-                        <td>South</td>
+                        <td>Ultimate tensile strain [usn]</td>
+                        <td>7875.167±793.89</td>
+                        <td>ASTM D3039/D3039M-17</td>
                     </tr>
                     <tr>
-                        <td>David</td>
-                        <td>Transport</td>
-                        <td>East</td>
+                        <td>Tensile modulus (chord 1000-3000 usn) [GPa]</td>
+                        <td>4.883±0.115</td>
+                        <td>ASTM D3039/D3039M-17</td>
+                    </tr>
+                    <tr>
+                        <td>Flexural strength [MPa]</td>
+                        <td>43.221±3.359</td>
+                        <td>ASTM D790-17</td>
+                    </tr>
+                    <tr>
+                        <td>Flexural modulus (chord 50-100 N) [MPa]</td>
+                        <td>590.944±41.81</td>
+                        <td>ASTM D790-17</td>
+                    </tr>
+                    <tr>
+                        <td>Maximum Load [kN]</td>
+                        <td>2.85±0.202</td>
+                        <td>ASTM D3039/D3039M-17</td>
+                    </tr>
+                    <tr>
+                        <td>Tensile strength [MPa]</td>
+                        <td>34.445±1.194</td>
+                        <td>ASTM D3039/D3039M-17</td>
+                    </tr>
+                    <tr>
+                        <td>Ultimate tensile strain [usn]</td>
+                        <td>7875.167±793.89</td>
+                        <td>ASTM D3039/D3039M-17</td>
+                    </tr>
+                    <tr>
+                        <td>Tensile modulus (chord 1000-3000 usn) [GPa]</td>
+                        <td>4.883±0.115</td>
+                        <td>ASTM D3039/D3039M-17</td>
+                    </tr>
+                    <tr>
+                        <td>Flexural strength [MPa]</td>
+                        <td>43.221±3.359</td>
+                        <td>ASTM D790-17</td>
+                    </tr>
+                    <tr>
+                        <td>Flexural modulus (chord 50-100 N) [MPa]</td>
+                        <td>590.944±41.81</td>
+                        <td>ASTM D790-17</td>
                     </tr>
                 </table>
             </div>
         </div>
-        
-        <br>
-        <br>
-        <hr class="line-section-2" size="10px">
-    </div>
-    <hr class="line-section-2" size="10px">
 
+        <br>
+        <br>
+        <hr class="line-section-2" size="5px">
+    </div>
 </template>
 <style lang="scss">
 // Import Main styles for this application
