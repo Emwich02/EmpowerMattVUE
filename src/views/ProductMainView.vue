9659473b3614606f60a60bb87b2fdd97ec8aa29b
wishmedew <115174006+wishmedew@users.noreply.github.com>
2023-11-01 01:02:35 +0700

    [productmain] เพิ่มข้อความ ใส่กล่อง เพิ่ม botton

--- src/views/ProductMainView.vue
+++ src/views/ProductMainView.vue
@@ -8,7 +8,7 @@ import bioCompositePic from '@/images/bioCompositePic.png';
 const Data = {
     whatis: 'What is ',
     hemp: 'Hemp',
-    firstparagraph: '	            Hemp has been cultivated in the north of Thailand by the hill tribes or the tribesman for long times. There is a typical hemp story in Thailand which is relevant to Hmong people. They believe that hemp plays an important role in their lifestyle and culture. In their houses, they have Hmong clothing made from hemp fiber. For ceremonies, they always wear Hmong clothing. Especially in funeral ceremonies, they use hemp clothing and shoes for the deceased. They believe that hemp fiber serves as a guide for the journey of their deceased ancestors.',
+    firstparagraph: '               Hemp has been cultivated in the north of Thailand by the hill tribes or the tribesman for long times. There is a typical hemp story in Thailand which is relevant to Hmong people. They believe that hemp plays an important role in their lifestyle and culture. In their houses, they have Hmong clothing made from hemp fiber. For ceremonies, they always wear Hmong clothing. Especially in funeral ceremonies, they use hemp clothing and shoes for the deceased. They believe that hemp fiber serves as a guide for the journey of their deceased ancestors.',
     secondparagraph:`               Nowadays, hemp is widely used in various industries around the world. Hemp is claimed to be a keyword for sustainable economy on our planet. Eco-friendly products are the core concept of our company that we share with our partners. With care, we meticulously select hemp materials from organic farms in Thailand to produce our high-quality products before delivering them to you.`,
     our: 'our ',
     products: 'products',
@@ -34,16 +34,41 @@ const Data = {
             <br><br><br> <img :src="downlight" class="img-fluid" style="max-width: 100%; height: auto;">   
         </div>
     </div>
-    </div><br><br><br> 
-
-    <div class="container">
-        <img :src="bioCompositePic" class="img-fluid" style="max-width: 50%; height: auto;">   
-        
-    </div><br><br> <br><br> <br><br> 
-    <div class="container">
-    <img :src="bioPlasticPic" class="img-fluid" style="max-width: 50%; height: auto;">   
     </div>
-    <br><br><br><br><br><br>
+
+    <div class="container full-height  ">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <img :src="bioCompositePic" class="img-fluid" style="max-width: 100%; height: auto;">
+                </div>
+
+                <div class="col-md-5 d-flex align-items-center">
+                    <div><br> 
+                        <p><span class="black-text">{{ Data.our }}</span>  <span class="green-text">{{ Data.products }}</span></p>
+                        <h5 class="blackbold-text">{{ Data.compositeTitle }}</h5>
+                        <p> {{ Data.compositeDetails }} </p>              
+                        <p class="button-details"  style="color:white"> {{ Data.moreDetails }}</p>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    
+        <div class="container full-height  ">
+            <div class="row justify-content-center">
+                <div class="col-md-5 d-flex align-items-center">
+                    <div><br> 
+                        <p><span class="black-text">{{ Data.our }}</span>  <span class="green-text">{{ Data.products }}</span></p>
+                        <h5 class="blackbold-text">{{ Data.plasticTitle }}</h5>
+                        <p> {{ Data.plasticDetails }} </p>
+                        <p class="button-details"  style="color:white"> {{ Data.moreDetails }}</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img :src="bioPlasticPic" class="img-fluid" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+        </div>
+    <br><br><br>
 </template>
 
 <style lang="scss">
