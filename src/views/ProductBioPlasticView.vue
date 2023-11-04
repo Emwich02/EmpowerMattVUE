44c4ece9c2a60c5375ff2bfe574c221f548be2f7
wishmedew <115174006+wishmedew@users.noreply.github.com>
2023-11-04 19:01:06 +0700

    [ProductMain,plastic][home] responsive +animation

--- src/views/ProductBioPlasticView.vue
+++ src/views/ProductBioPlasticView.vue
@@ -13,38 +13,40 @@ const Data = {
     HempBioPlastics: 'Hemp-bioplastics ',
     CompoundMaterials: 'compound materials',
     HempBioPlasticsDetails: `There are some hemp-bioplastic pellets and food utensil products such as straw, flock, spoon and shop stick.`,
-    straw:'straw',
-    cup:'cup',
-    SpoonAndFork:'spoon & fork',
+    straw: 'straw',
+    cup: 'cup',
+    SpoonAndFork: 'spoon & fork',
 }
 </script>
 
 <template>
-<div class="background-cream">
-<br><br><br><br>
+    <div class="background-cream">
+        <br><br><br><br>
 
-    <div class="container">
-        <div class="box mt-5 col-2" style="margin-left: 0px;">
-            <h4 class="text-center" style="color:white">{{ Data.Products }} </h4>
-        </div><br>
+        <div class="container">
+            <div class="box mt-5  " style="margin-right: 70%;" data-aos="fade-up">
+                <h4 class="text-center" style="color:white">{{ Data.Products }} </h4>
+            </div><br>
 
-            <div class="text-center"> 
-                <h4 class="blackbold-text">{{ Data.Our }} <span class="greenbold-text">{{ Data.product }} </span> </h4> 
-                <h2 class="greenbold-text"> {{ Data.HempBioPlastics }} <span class="blackbold-text">{{ Data.CompoundMaterials }} </span> </h2> <br>
-                <p>{{ Data.HempBioPlasticsDetails }}</p><br>
-                <img :src="Bioplastic" class="img-fluid" style="max-width: 90%; height: auto;"><br><br><br><br>
-                <img :src="downlight" class="img-fluid" style="max-width: 100%; height: auto;"><br><br><br><br><br><br>
-                <hr class="style1"><br><br><br><br>
-                <h2 class="greenbold-text"> {{ Data.HempBioPlastics }} <span class="blackbold-text">{{ Data.product }} </span> </h2> 
+            <div class="text-center">
+                <h4 class="blackbold-text front-15px" data-aos="fade-up">{{ Data.Our }} <span class="greenbold-text">{{ Data.product }} </span> </h4>
+                
+                    <h2 class="greenbold-text front-17px" data-aos="fade-up"> {{ Data.HempBioPlastics }} <span class="blackbold-text">{{
+                        Data.CompoundMaterials }} </span> </h2>
+               <br>
+                <p class="front-13px" data-aos="fade-up">{{ Data.HempBioPlasticsDetails }}</p><br>
+                <img :src="Bioplastic" class="img-fluid" style="max-width: 90%; height: auto;" data-aos="fade-up"><br><br><br><br>
+                <img :src="downlight" class="img-fluid" style="max-width: 100%; height: auto;" data-aos="fade-up"><br><br><br><br>
+                <hr class="style1" data-aos="fade-up"><br><br><br>
+                <h2 class="greenbold-text front-17px" data-aos="fade-up"> {{ Data.HempBioPlastics }} <span class="blackbold-text">{{ Data.product }}
+                    </span> </h2>
                 <br><br><br>
-                <img :src="Plasticproducts" class="img-fluid" style="max-width: 80%; height: auto;"><br><br><br>
-            </div> 
+                <img :src="Plasticproducts" class="img-fluid" style="max-width: 80%; height: auto;" data-aos="fade-up"><br><br><br>
+            </div>
         </div>
 
-</div>
+    </div>
 </template>
 
-<style lang="scss">
-// Import Main styles for this application
-@import '/src/assets/productBioPlastic.scss';
-</style>
+<style lang="scss">// Import Main styles for this application
+@import '/src/assets/productBioPlastic.scss';</style>
