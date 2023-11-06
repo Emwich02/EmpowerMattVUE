448060f2103d4b730296bae0686f0fb750e02a2e
wishmedew <115174006+wishmedew@users.noreply.github.com>
2023-11-06 22:12:42 +0700

    [hempfabric] เพิ่มภาษา ปรับรูปbg

--- src/views/NaturalHempFabricView.vue
+++ src/views/NaturalHempFabricView.vue
@@ -6,15 +6,19 @@ import fabric from '@/images/Hempfabric/fabric.png';
 import aktableone from '@/images/Hempfabric/aktableone.png';
 import aktabletwo from '@/images/Hempfabric/aktabletwo.png';
 import aktablethree from '@/images/Hempfabric/aktablethree.png';
+import hempcar from '@/images/Hempfabric/hempcar.png';
+import { computed, reactive } from 'vue'
+import { useLanguage } from '/src/stores/useLanguage.js'
+const languageStores = useLanguage()
+const isLang = computed(() => languageStores.activeLang)
 
-
-const Data = {
-    Title: 'Products',
-    Our: 'our',
-    Product: 'product',
-    Natural: 'Natural',
-    HempFabric: 'Hemp Fabric',
-    Descriptionmain: `           Natural hemp fabrics are reinforcement textiles based on natural hemp fibre and others such
+const Data = reactive({
+    Title: computed(() => isLang.value == 'THA' ? 'Products' : 'Products'),
+    Our: computed(() => isLang.value == 'THA' ? 'พบกับทีมของพวกเรา' : 'our'),
+    Product: computed(() => isLang.value == 'THA' ? '1111' : 'Product'),
+    Natural: computed(() => isLang.value == 'THA' ? 'ไอยู' : "Natural"),
+    HempFabric: computed(() => isLang.value == 'THA' ? 'ไอยู' : "Hemp Fabric"),
+    Descriptionmain: computed(() => isLang.value == 'THA' ? 'ไอยู' : `           Natural hemp fabrics are reinforcement textiles based on natural hemp fibre and others such
 as polypropylene fibre, recycled polyethylene terephthalate fiber, glass fiber and cotton fibre which
 are designed for fibre-reinforced polymer composite applications. The fabrics can be used with a
 large variety of resins in processes including wet lay-up, vacuum infusion, resin transfer moulding
@@ -22,9 +26,9 @@ large variety of resins in processes including wet lay-up, vacuum infusion, resi
 technique to glass or carbon fibre. Hybrid Hemp-PP reinforcements provide a high performance, an
 easy processing, a lightweight and an environmental impact. They are suitable for semi-structural
 and decorative applications in sectors such as automotive, construction, marine, sports and
-consumer goods.`,
-    SolarSatellitePanels: 'Solar Satellite Panels',
-    DescriptionSolarSatellitePanels: `Hemp and recycled polyethylene terephthalate
+consumer goods.`),
+    SolarSatellitePanels: computed(() => isLang.value == 'THA' ? 'ไอยู' : "Solar Satellite Panels"),
+    DescriptionSolarSatellitePanels: computed(() => isLang.value == 'THA' ? 'ไอยู' : `Hemp and recycled polyethylene terephthalate
 (rPET) composite materials consisted of
 approximately 20 wt% natural hemp fiber. Then,
 the composite materials were studied to fabricate
@@ -33,12 +37,14 @@ satellite panels. This research project is the
 collaboration between Empowermatt Co.,Ltd.
 Hemp Thai Co.,Ltd., Thai Taffita Co.,Ltd., and
 GALAXI (GISTDA’s Aerospace Laboratory of
-Excellence and Innovation), GISTDA.`,
-    DescriptionNaturalhempfabric: `Natural hemp fabric for bio-composite materials, 
-making possible lightweight structure.`,
-    Propertiesof: 'Properties of ',
-    AKNaturalFabric: `'AK Natural Fabric'`
-}
+Excellence and Innovation), GISTDA.`),
+    DescriptionNaturalhempfabric: computed(() => isLang.value == 'THA' ? 'ไอยู' : `Natural hemp fabric for bio-composite materials, 
+making possible lightweight structure.`),
+    Propertiesof: computed(() => isLang.value == 'THA' ? 'ไอยู' : "Properties of "),
+    AKNaturalFabric: computed(() => isLang.value == 'THA' ? 'ไอยู' : `'AK Natural Fabric'`),
+});
+
+
 function scrollToSection(sectionId) {
     const element = document.getElementById(sectionId);
     if (element) {
@@ -87,37 +93,39 @@ export default {
 }
 </script> -->
 <template>
-    <div class="bgpic full-height" :style="{ backgroundImage: 'url(src/images/Hempfabric/hempcar.png)' }"
-        style="margin-top: 2rem; " >
+    <div class="bgpic" v-bind:style="{ backgroundImage: 'url(' + hempcar + ')' }" style="margin-top: 2rem; ">
         <div class="container">
             <div class="row">
                 <div class="box mt-5 col-2" style="margin-left: 0px;">
-                    <h4 class="text-center" style="color:white">{{ Data.Title }} </h4>
+                    <h4 class="text-center " style="color:white">{{ Data.Title }} </h4>
                 </div>
                 <div class="row pt-5">
                     <div class="col-md-1"></div>
                     <div class="col-md-11">
                         <h4 class="front-17px">{{ Data.Our }} <span class="greencolor">{{ Data.Product }}</span></h4>
-                        <h2 class="front-20px"><span class="greencolor ">{{ Data.Natural }}</span> {{ Data.HempFabric }}</h2>
-                        <p class="front-13px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ Data.Descriptionmain }}</p>
+                        <h2 class="front-20px"><span class="greencolor ">{{ Data.Natural }}</span> {{ Data.HempFabric }}
+                        </h2>
+                        <p class="front-13px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{
+                            Data.Descriptionmain }}</p>
                     </div>
                 </div>
             </div>
-            <div class="text-center " >
+            <div class="text-center ">
                 <span style="cursor: pointer;  " @click="scrollToSection('first')">↓ ↓ ↓</span>
+                <br><br><br>
             </div>
         </div>
 
-    </div>
+    </div><br><br>
 
     <div id="first">
         <div class="container ps-5 full-height  ">
             <div class="row">
                 <div class="col-md-6 d-flex align-items-center">
                     <div>
-                        <h4><span class="greencolor front-17px">{{ Data.Natural }}</span> {{ Data.HempFabric }}</h4>
-                        <h2 class="greencolor front-17px">{{ Data.SolarSatellitePanels }}</h2>
-                        <p class="front-13px" >{{ Data.DescriptionSolarSatellitePanels }}</p>
+                        <h4 class="front-17px"><span class="greencolor">{{ Data.Natural }}</span> {{ Data.HempFabric }}</h4>
+                        <h2 class="greencolor front-20px">{{ Data.SolarSatellitePanels }}</h2>
+                        <p class="front-13px">{{ Data.DescriptionSolarSatellitePanels }}</p>
                     </div>
                 </div>
                 <div class="col-md-6">
@@ -149,23 +157,24 @@ export default {
 
     </div>
     <div id="third">
-        <div class="container ps-5 full-height  ">
-            <div class="row d-flex align-items-center">
-                <div>
-                    <h4 class="front-17px"><span class="greencolor ">{{ Data.Propertiesof }}</span> {{ Data.AKNaturalFabric }}</h4>
-                </div>
-                <div class="text-center">
-                    <div class="mt-5 item">
-                        <img :src="aktableone" class="img-fluid" style="max-width: 100%; height: auto;" >
-                    </div>
-                    <div class="mt-5">
-                        <img :src="aktabletwo" class="img-fluid"  style="max-width: 100%; height: auto;">
-                    </div>
-                    <div class="mt-5">
-                        <img :src="aktablethree" class="img-fluid"  style="max-width: 100%; height: auto;">
-                    </div>
-                </div>
+
+        <div class="container">
+            <h4 class="front-17px"><span class="greencolor ">{{ Data.Propertiesof }}</span> {{ Data.AKNaturalFabric
+            }}</h4><br>
+        </div>
+        <div class="container d-flex align-items-center justify-content-center">
+            <div>
+                <div class="col-md-5 " data-aos="fade-up">
+                    <img :src="aktableone" class="img-fluid" style="max-width: 100%; height: auto;">
+                </div><br>
+                <div class="col-md-5" data-aos="fade-up">
+                    <img :src="aktabletwo" class="img-fluid" style="max-width: 100%; height: auto;">
+                </div><br>
+                <div class="col-md-5" data-aos="fade-up">
+                    <img :src="aktablethree" class="img-fluid" style="max-width: 100%; height: auto;">
+                </div><br><br>
             </div>
+
         </div>
     </div>
 </template>
