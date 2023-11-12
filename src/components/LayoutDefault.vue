ff218afd19f75d251cc848854ade072d877caa3c
wishmedew <115174006+wishmedew@users.noreply.github.com>
2023-11-12 21:59:09 +0700

    [Layoutdefult] fixed foot responsive

--- src/components/LayoutDefault.vue
+++ src/components/LayoutDefault.vue
@@ -119,30 +119,62 @@ onMounted(() => {
 
     <footer class="text-center text-lg-start bg-cream text-muted">
         <section class="pt-2 pb-2">
-            <div class="container text-center text-md-start mt-5">
-                <div class="row mt-3">
-                    <div class="col-md-3 col-lg-5 col-xl-4 mx-auto ">
+            <div class="first">
+                <div class="container text-center text-md-start mt-5">
+                    <div class="row mt-3">
+                        <div class="col-md-3 col-lg-5 col-xl-4 mx-auto ">
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                {{ CONSTANT_WORD.Desctiption }}
+                            </h6>
+                            <p>
+                                <img :src="tell" class="img-fluid" style="max-width: 100%; height: 21px;"> <i
+                                    class="bi bi-telephone-fill"></i> {{ CONSTANT_WORD.Tel }}
+                            </p>
+                            <p>
+                                <img :src="email" class="img-fluid" style="max-width: 100%; height: 16px;">{{
+                                    CONSTANT_WORD.Email }}
+                            </p>
+                        </div>
+                        <div class="col-md-3 col-lg-5 col-xl-4 mx-auto mb-4">
+                            <br><br>
+                            <p>
+                                <img :src="tax" class="img-fluid" style="max-width: 100%; height: 19px;"> {{
+                                    CONSTANT_WORD.Tax }}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-9 col-lg-11 col-xl-10 mx-auto mb-4">
+                        <p>
+                            <img :src="address" class="img-fluid"
+                                style="max-width: 100%; height: 20px; padding-left: 3px;">{{ CONSTANT_WORD.Address }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="second">
+                <div class="container text-center text-md-start mt-5">
+                    <div class="row mt-3">
                         <h6 class="text-uppercase fw-bold mb-4">
                             {{ CONSTANT_WORD.Desctiption }}
                         </h6>
                         <p>
-                            <img :src="tell" class="img-fluid" style="max-width: 100%; height: 21px;"> <i class="bi bi-telephone-fill"></i> {{ CONSTANT_WORD.Tel }}
+                            <img :src="tell" class="img-fluid" style="max-width: 100%; height: 21px;"> <i
+                                class="bi bi-telephone-fill"></i> {{ CONSTANT_WORD.Tel }}
                         </p>
-                        
                         <p>
-                            <img :src="email" class="img-fluid" style="max-width: 100%; height: 16px;">{{ CONSTANT_WORD.Email }}
+                            <img :src="email" class="img-fluid" style="max-width: 100%; height: 16px;">{{
+                                CONSTANT_WORD.Email }}
                         </p>
-                    </div>
-                    <div class="col-md-3 col-lg-5 col-xl-4 mx-auto mb-4">
                         <br><br>
                         <p>
-                            <img :src="tax" class="img-fluid" style="max-width: 100%; height: 19px;"> {{ CONSTANT_WORD.Tax }}
+                            <img :src="tax" class="img-fluid" style="max-width: 100%; height: 19px;"> {{
+                                CONSTANT_WORD.Tax }}
                         </p>
                     </div>
-                </div>
-                <div class="col-md-9 col-lg-11 col-xl-10 mx-auto mb-4">
                     <p>
-                        <img :src="address" class="img-fluid" style="max-width: 100%; height: 20px; padding-left: 3px;">{{ CONSTANT_WORD.Address }}
+                        <img :src="address" class="img-fluid" style="max-width: 100%; height: 20px; padding-left: 3px;">{{
+                            CONSTANT_WORD.Address }}
                     </p>
                 </div>
             </div>
@@ -152,38 +184,42 @@ onMounted(() => {
 <style lang="scss">
 // Import Main styles for this application
 @import '/src/assets/layoutDefault.scss';
+
 @keyframes staggeredSlideIn {
     0% {
-      opacity: 0;
-      transform: translateY(-10px);
+        opacity: 0;
+        transform: translateY(-10px);
     }
+
     25% {
-      opacity: 1;
-      transform: translateY(0);
+        opacity: 1;
+        transform: translateY(0);
     }
+
     50% {
-      opacity: 1;
-      transform: translateY(0);
+        opacity: 1;
+        transform: translateY(0);
     }
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
 
-  .dropdown-menu {
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+.dropdown-menu {
     animation: staggeredSlideIn 1s ease-in-out;
-  }
+}
 
-  @keyframes staggeredItemFadeIn {
+@keyframes staggeredItemFadeIn {
     0% {
-      opacity: 0;
-      transform: translateY(-10px);
+        opacity: 0;
+        transform: translateY(-10px);
     }
+
     100% {
-      opacity: 1;
-      transform: translateY(0);
+        opacity: 1;
+        transform: translateY(0);
     }
-  }
-</style>
+}</style>
   
